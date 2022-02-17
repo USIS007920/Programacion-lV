e67adb650597e4cfd2159c1ead02eb4cfcc98ce6
Fernando Antonio Quintanilla Soriano <usis007920@ugb.edu.sv>
2022-02-17 10:03:37 -0600

    Primer proyecto

--- primer proyecto/main.js
+++ primer proyecto/main.js
@@ -1,52 +1,23 @@
-var db_sistema = openDatabase('dbsistema', '1.0', 'Sistema de Facturacion', 5 * 1024 * 1024);
-if(!db_sistema){
-    alert('Lo siento tu navegado NO soporta BD locales.');
-}
-var app = new Vue({
-    el: '#appCliente',
+var generarIdUnicoFecha = ()=>{
+    let fecha = new Date();
+    return Math.floor(fecha.getTime()/1000).toString(16);
+};
+var appSistema = new Vue({
+    el: '#appSistema',
     data: {
-        cliente: {
-            accion: '',
-            msg : '',
-            idCliente: '',
-            codigo: '',
-            nombre: '',
-            direccion: '',
-            telefono: '',
-            dui: ''
-        },
-    },
-    methods: {
-        guardarCliente(){
-            db_sistema.transaction(tx=>{
-                tx.executeSql('INSERT INTO clientes (codigo, nombre, direccion, telefono, dui) VALUES (?,?,?,?,?)',
-                [this.cliente.codigo, this.cliente.nombre, this.cliente.direccion, this.cliente.telefono, 
-                    this.cliente.dui],
-                (tx, results)=>{
-                    this.cliente.msg = 'Cliente guardado con exito';
-                    this.nuevoCliente();
-                },
-                (tx, error)=>{
-                    this.cliente.msg = `Error al guardar el cliente ${error.message}`;
-                });
-            });
-        },
-        nuevoCliente(){
-            this.cliente.accion = 'nuevo';
-            this.cliente.idCliente = '';
-            this.cliente.codigo = '';
-            this.cliente.nombre = '';
-            this.cliente.direccion = '';
-            this.cliente.telefono = '';
-            this.cliente.dui = '';
+        forms:{
+            'cliente':{mostrar:false},
+            'producto':{mostrar:false},
+            'proveedor':{mostrar:false},
+            'categoria':{mostrar:false}
         }
     },
-    created(){
-        db_sistema.transaction(tx=>{
-            tx.executeSql('CREATE TABLE IF NOT EXISTS clientes(idCliente INTEGER PRIMARY KEY AUTOINCREMENT, '+
-                'codigo char(10), nombre char(75), direccion TEXT, telefono char(10), dui char(10))');
-        }, err=>{
-            console.log('Error al crear la tabla de clientes', err);
+});
+document.addEventListener('DOMContentLoaded', e=>{
+    let formularios = document.querySelectorAll('.mostrar').forEach(formulario=>{
+        formulario.addEventListener('click', evento=>{
+            let formulario = evento.target.dataset.form;
+            appSistema.forms[formulario].mostrar = true;
         });
-    }
+    });
 });
